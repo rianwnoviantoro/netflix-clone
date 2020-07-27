0a68f93e86f16c264aec65e8bfd740fadb16ef24
Mongoding <tenkeylost@gmail.com>
2020-07-27 21:30:31 +0700

    if isLargeRow

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import requests from './request'
 
 function App() {
    return (
-      <div className="App">
+      <div className="app">
          <h1>Test</h1>
-         <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+         <Row
+            title="NETFLIX ORIGINALS"
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+         />
          <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+         <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+         <Row title="Documentaries Movies" fetchUrl={requests.fetchDocumentaries} />
       </div>
    )
 }
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -4,7 +4,7 @@ import './Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
    const [movies, setMovies] = useState([]);
 
    // Kode snippet dimana akan berjalan berdasarkan kondisi tertentu (requestUrl)
@@ -31,8 +31,8 @@ function Row({ title, fetchUrl }) {
             {movies.map(movie => (
                <img
                   key={movie.id}
-                  className="row__poster"
-                  src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                  className={`row__poster ${isLargeRow && "row_posterLarge"}`}
+                  src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
             ))}
          </div>
          {/* container -> posters */}
