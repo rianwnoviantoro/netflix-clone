f8e94b7f1227488c24e6a559355e8badb28ea9b9
Mongoding <tenkeylost@gmail.com>
2020-07-27 21:01:02 +0700

    fetch image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
+import Row from './components/Row/Row'
+import requests from './request'
 
 function App() {
    return (
       <div className="App">
          <h1>Test</h1>
+         <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+         <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       </div>
    )
 }
---
+++ src/components/Row/Row.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import axios from '../../axios'
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+   const [movies, setMovies] = useState([]);
+
+   // Kode snippet dimana akan berjalan berdasarkan kondisi tertentu (requestUrl)
+   useEffect(() => {
+      // Jika [], jalankan / tampilkan row 1 kali
+      async function fetchData() {
+         const request = await axios.get(fetchUrl); // fetch data berdasarkan url dari App.js
+         // console.log(request.data.results);
+         setMovies(request.data.results);
+         return request;
+      }
+      fetchData();
+   }, [fetchUrl]);
+
+   // console.log(movies);
+
+   return (
+      <div className="row">
+         {/* TItle */}
+         <h2>{title}</h2>
+         <div className="row__posters">
+            {/* beberapa row_poster */}
+
+            {movies.map(movie => (
+               <img
+                  className="row__poster"
+                  src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+            ))}
+         </div>
+         {/* container -> posters */}
+
+      </div>
+   )
+}
+
+export default Row
